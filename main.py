3fbc90cd8a1e9cd644e8565c3b6c64d211473e66
Maliyagithub <129000805+Maliyagithub@users.noreply.github.com>
2023-03-27 12:52:07 +0530

    Update main.py

--- main.py
+++ main.py
@@ -7,12 +7,12 @@ bot = telebot.TeleBot("5953671984:AAG9XaarXNDyM0axBHZVTLjtnjwQ6bpIcTE")
 
 @bot.message_handler(commands=["start"])
 def send_welcome(message):
-  bot.reply_to(message, "Hello! I'm Uvindu Bro Chat Bot")
+  bot.reply_to(message, "Hello! I'm maliya Bro Chat Bot")
 
 
 @bot.message_handler(commands=["how"])
 def send_message(message):
-  bot.send_message(message.chat.id, "https://youtube.com/c/Uvindubro")
+  bot.send_message(message.chat.id, "https://drive.google.com/uc?export=download&id=1BJfGsK-2iPf4wfTScjF5zB0qN3k0fZM2")
 
 
 
